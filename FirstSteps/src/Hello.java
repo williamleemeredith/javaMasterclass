d862e206c458f7777893c1a26d4578d95958765f
WilliamMeredith <williamleemeredith@gmail.com>
2020-12-16 13:57:12 -0500

    primative types

--- FirstSteps/src/Hello.java
+++ FirstSteps/src/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
         System.out.println("Hello, Bill");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
