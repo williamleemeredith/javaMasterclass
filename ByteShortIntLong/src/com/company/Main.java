d41af3fd893f82623f4056f93ab63ccc1fed886f
WilliamMeredith <williamleemeredith@gmail.com>
2020-12-17 15:51:32 -0500

    primitives

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -9,5 +9,25 @@ public class Main {
 	    int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min Value = " + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Min Byte Value = " + myMinByteValue);
+        System.out.println("Max Byte Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Min Short Value = " + myMinShortValue);
+        System.out.println("Max Short Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Min Long Value = " + myMinLongValue);
+        System.out.println("Max Long Value = " + myMaxLongValue);
+
+        short bigSHortLiteralValue = 32767;
     }
 }
